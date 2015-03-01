cc18a3419621b0a1f593eb7774a99c4ba67c78a6
Shipeng Feng <fsp261@gmail.com>
2015-03-01 17:40:44 +0800

    Added tests

---
+++ tests/test_basic.rs
@@ -0,0 +1,8 @@
+// Test the basic functionality.
+// Copyright (c) 2015 by Shipeng Feng.
+// Licensed under the BSD License, see LICENSE for more details.
+
+#[test]
+fn foo() {
+    assert!(true);
+}
