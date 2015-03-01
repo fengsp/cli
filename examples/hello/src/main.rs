0f555c891db65e5fc44d655e36c9493ff9b41129
Shipeng Feng <fsp261@gmail.com>
2015-03-01 22:49:17 +0800

    Added Command

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -1,3 +1,17 @@
+extern crate cli;
+
+use cli::Cli;
+
+
+/// Simple program that greets NAME.
+fn hello(name: String) {
+    println!("Hello {}", name);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = Cli::new();
+    app.command(hello);
+    app.option("--name", "Your name", "The person to greet.");
+    app.run();
 }
--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,10 @@
-// Rust command line utility.
+// Core types.
 // Copyright (c) 2015 by Shipeng Feng.
 // Licensed under the BSD License, see LICENSE for more details.
 
 
-/// The cli type.
-pub struct Cli;
+/// The command is the basic type of command line applications in cli.  This
+/// handles command line parsing.
+pub struct Command {
+    name: String,
+}
