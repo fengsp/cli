4e812b47ee5854e4f61b2bb2538e30d835515049
Shipeng Feng <fsp261@gmail.com>
2015-03-01 18:00:10 +0800

    Initialize lib.rs

---
+++ src/core.rs
@@ -0,0 +1,7 @@
+// Rust command line utility.
+// Copyright (c) 2015 by Shipeng Feng.
+// Licensed under the BSD License, see LICENSE for more details.
+
+
+/// The cli type.
+pub struct Cli;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-#[test]
-fn it_works() {
-}
+// Rust command line utility.
+// Copyright (c) 2015 by Shipeng Feng.
+// Licensed under the BSD License, see LICENSE for more details.
+
+//! Cli is a Rust crate for creating beautiful command line applications.
+
+#![crate_name = "cli"]
+#![crate_type = "lib"]
+#![deny(non_camel_case_types)]
+
+/* public api */
+pub use core::Cli;
+
+mod core;
