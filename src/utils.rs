17c4c8c97c1bcb09b70a8b11e0bc7944023f8211
Shipeng Feng <fsp261@gmail.com>
2015-03-07 01:40:24 +0800

    Test style

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@
 // Copyright (c) 2015 by Shipeng Feng.
 // Licensed under the BSD License, see LICENSE for more details.
 
+use std::io;
 
 pub fn print(message: String) {
     print!("{}", message);
@@ -10,6 +11,11 @@ pub fn print(message: String) {
 
 pub fn style(text: String, fg: &str, bg: &str, bold: bool, underline: bool,
              blink: bool, reverse: bool, reset: bool) -> String {
-    let mut bits: Vec<u8> = Vec::new();
-    "style".to_string()
+    let mut bytes: Vec<u8> = Vec::new();
+    bytes.push(27 as u8);
+    bytes.push('[' as u8);
+    bytes.push(32 as u8);
+    bytes.push('m' as u8);
+    bytes.push_all(text.as_bytes());
+    io::stdout().write(bytes);
 }
