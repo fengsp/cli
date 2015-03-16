c4bf7faee9a0bde12008804c89a7b2e05ba26461
Shipeng Feng <fsp261@gmail.com>
2015-03-16 15:47:24 +0800

    Added docs

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['cli'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='cli/struct.Style.html' title='cli::Style'>Style</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
